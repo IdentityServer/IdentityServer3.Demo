8dc17bdce4c8a17d1c7aabfd929b8b533b1b0d85
Dominick Baier <dbaier@leastprivilege.com>
2016-02-27 17:03:37 +0100

    remove unused packages

--- AzureWebSitesDeployment/Startup.cs
+++ AzureWebSitesDeployment/Startup.cs
@@ -1,9 +1,6 @@
 ﻿using AzureWebSitesDeployment.Config;
-using Microsoft.Owin.Security.Google;
-using Microsoft.Owin.Security.OpenIdConnect;
 using Owin;
 using IdentityServer3.Core.Configuration;
-using IdentityServer3.Core.Logging;
 using IdentityServer3.Core.Services;
 
 namespace AzureWebSitesDeployment
@@ -29,32 +26,5 @@ namespace AzureWebSitesDeployment
 
             app.UseIdentityServer(idsrvOptions);            
         }
-
-        //public static void ConfigureIdentityProviders(IAppBuilder app, string signInAsType)
-        //{
-        //    var google = new GoogleOAuth2AuthenticationOptions
-        //    {
-        //        AuthenticationType = "Google",
-        //        Caption = "Google",
-        //        SignInAsAuthenticationType = signInAsType,
-
-        //        ClientId = "767400843187-8boio83mb57ruogr9af9ut09fkg56b27.apps.googleusercontent.com",
-        //        ClientSecret = "5fWcBT0udKY7_b6E3gEiJlze"
-        //    };
-        //    app.UseGoogleAuthentication(google);
-
-        //    var aad = new OpenIdConnectAuthenticationOptions
-        //    {
-        //        AuthenticationType = "aad",
-        //        Caption = "Azure AD",
-        //        SignInAsAuthenticationType = signInAsType,
-
-        //        Authority = "https://login.windows.net/4ca9cb4c-5e5f-4be9-b700-c532992a3705",
-        //        ClientId = "65bbbda8-8b85-4c9d-81e9-1502330aacba",
-        //        RedirectUri = "https://identity.thinktecture.com/aadcb"
-        //    };
-
-        //    app.UseOpenIdConnectAuthentication(aad);
-        //}
     }
 }
